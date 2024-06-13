a7bed04885c03aa2b793490f676eb9b054d86745
Richard Kosegi <richard.kosegi@gmail.com>
2024-06-13 20:58:57 +0200

    New: Path abstraction
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>

---
+++ path/builder_test.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2024 Richard Kosegi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package path
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBuilder(t *testing.T) {
+	b := NewBuilder().
+		Append(Simple("root")).
+		Append(Wildcard()).
+		Append(Numeric(1)).
+		Append(AfterLast())
+	p := b.Build()
+
+	pc := p.Components()
+
+	assert.False(t, p.IsEmpty())
+	assert.Equal(t, 4, len(pc))
+	assert.Equal(t, "root", pc[0].Value())
+	assert.True(t, pc[1].IsWildcard())
+	assert.True(t, pc[2].IsNumeric())
+	assert.Equal(t, "1", pc[2].Value())
+	assert.Equal(t, 1, pc[2].NumericValue())
+	assert.True(t, pc[3].IsInsertAfterLast())
+	assert.False(t, p.Last().IsNumeric())
+
+	b.Reset()
+	assert.True(t, b.Build().IsEmpty())
+}
+
+func TestBuilderAppendNoOption(t *testing.T) {
+	defer func() {
+		recover()
+	}()
+	NewBuilder().Append()
+	assert.Fail(t, "should not be here")
+}
+
+func TestPathGetLastEmpty(t *testing.T) {
+	defer func() {
+		recover()
+	}()
+	NewBuilder().Build().Last()
+	assert.Fail(t, "should not be here")
+}
---
+++ path/types.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2024 Richard Kosegi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package path
+
+// Builder provides convenient way to construct Path using fluent builder pattern.
+type Builder interface {
+	// Append adds path component to the end of path.
+	Append(opts ...AppendOpt) Builder
+
+	// Reset clears internal state of builder.
+	// Path instances created using this builder previously are unaffected by this operation.
+	Reset() Builder
+
+	// Build creates Path using current state.
+	// It's safe to call this function multiple times or re-use it afterward,
+	// it will always create fresh Path everytime.
+	Build() Path
+}
+
+type Component interface {
+	// IsInsertAfterLast returns true if this path component points to non-existent item after last element in the list.
+	// This is required by JSON pointer (rfc6901) during append to the end of list.
+	IsInsertAfterLast() bool
+
+	// IsNumeric returns true if name is numeric value according to rfc6901
+	IsNumeric() bool
+
+	// NumericValue gets number that points to array element with the zero-based index.
+	// Only valid if IsNumeric returns true.
+	NumericValue() int
+
+	// IsWildcard returns true if this path element represent wildcard match, ie equal to any value.
+	// This is used e.g. in property paths such as "x.y.z.*.w"
+	IsWildcard() bool
+
+	// Value returns canonical value of this component.
+	Value() string
+}
+
+type Path interface {
+	// Components returns copy of path components in this path
+	Components() []Component
+
+	// IsEmpty returns true if Path does not have any components.
+	IsEmpty() bool
+
+	// Last gets very last path Component, panics if path is empty.
+	Last() Component
+}
+
+type AppendOpt func(*component)
