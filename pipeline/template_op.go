1adad9eb3d62eb5f4425954c46056ef156841c65
Richard Kosegi <richard.kosegi@gmail.com>
2025-04-13 14:54:19 +0200

    Pipeline: Add ability to parse output of template op
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>

--- pipeline/template_op.go
+++ pipeline/template_op.go
@@ -21,6 +21,16 @@ import (
 	"strings"
 
 	"github.com/rkosegi/yaml-toolkit/dom"
+	"gopkg.in/yaml.v3"
+)
+
+type ParseTextAs string
+
+const (
+	// ParseTextAsNone do not parse, it's just a text (dom.Leaf)
+	ParseTextAsNone ParseTextAs = "none"
+	// ParseTextAsYaml parse text as a YAML source into dom.Node
+	ParseTextAsYaml ParseTextAs = "yaml"
 )
 
 // TemplateOp can be used to render value from data at runtime.
@@ -29,6 +39,9 @@ type TemplateOp struct {
 	Template string `yaml:"template"`
 	// path within global data tree where to set result at
 	Path string `yaml:"path" clone:"template"`
+	// How to treat rendered text after template engine completes successfully.
+	// It's responsibility of template to produce source that is parseable by chosen mode
+	ParseAs *ParseTextAs `yaml:"parseAs,omitempty" clone:"text"`
 	// Trim when true, whitespace is trimmed off the value
 	Trim *bool `yaml:"trim,omitempty"`
 }
@@ -49,7 +62,23 @@ func (ts *TemplateOp) Do(ctx ActionContext) error {
 	if safeBoolDeref(ts.Trim) {
 		val = strings.TrimSpace(val)
 	}
-	ctx.Data().AddValueAt(ctx.TemplateEngine().RenderLenient(ts.Path, ss), dom.LeafNode(val))
+	if ts.ParseAs == nil {
+		ts.ParseAs = ptr(ParseTextAsNone)
+	}
+	var node dom.Node
+	switch *ts.ParseAs {
+	case ParseTextAsYaml:
+		var yn yaml.Node
+		if err = yaml.Unmarshal([]byte(val), &yn); err != nil {
+			return err
+		}
+		node = dom.YamlNodeDecoder()(&yn)
+	case ParseTextAsNone:
+		node = dom.LeafNode(val)
+	default:
+		return fmt.Errorf("unknown ParseAs mode: %v", *ts.ParseAs)
+	}
+	ctx.Data().AddValueAt(ctx.TemplateEngine().RenderLenient(ts.Path, ss), node)
 	return err
 }
 
