45d45dd096a5ef7276e16e0a7eb08ddf2fc3eeb9
Richard Kosegi <richard.kosegi@gmail.com>
2026-08-17 16:54:09 +0200

    🚚 Migrate gopkg.in/yaml.v3 to go.yaml.in/yaml/v4
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>

--- dom/codec_test.go
+++ dom/codec_test.go
@@ -22,7 +22,7 @@ import (
 
 	"github.com/rkosegi/yaml-toolkit/common"
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v3"
+	"go.yaml.in/yaml/v4"
 )
 
 func TestDecodeYamlNode(t *testing.T) {
--- k8s/manifest.go
+++ k8s/manifest.go
@@ -24,7 +24,7 @@ import (
 	"os"
 
 	"github.com/rkosegi/yaml-toolkit/common"
-	"gopkg.in/yaml.v3"
+	"go.yaml.in/yaml/v4"
 )
 
 const (
